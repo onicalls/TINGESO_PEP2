bbbde5f4e68f456c8ea90407fe5f2e505e1839f9
Nicolás Rojas <64169554+onicalls@users.noreply.github.com>
2023-06-15 17:04:17 -0400

    backend setup

--- valorleche-service/src/main/java/com/tingeso/valorlecheservice/controllers/ValorLecheController.java
+++ valorleche-service/src/main/java/com/tingeso/valorlecheservice/controllers/ValorLecheController.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/acopio")
+@RequestMapping("/valorleche")
 public class ValorLecheController {
 
     @Autowired
--- valorleche-service/src/test/java/com/tingeso/valorlecheservice/ValorLecheServiceApplicationTests.java
+++ valorleche-service/src/test/java/com/tingeso/valorlecheservice/ValorLecheServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MarcasRelojServiceApplicationTests {
+class ValorLecheServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
